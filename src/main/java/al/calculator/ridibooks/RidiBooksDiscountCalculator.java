ab61b8532b8c34b8c77578a7ad167a1be3463d3b
joke111 <alta.second@gmail.com>
2019-08-25 21:01:43 +0900

    initialize

---
+++ src/main/java/al/calculator/ridibooks/RidiBooksDiscountCalculator.java
@@ -0,0 +1,13 @@
+package al.calculator.ridibooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidiBooksDiscountCalculator {
+
+	public static void main(String[] args) {
+		new SpringApplication(RidiBooksDiscountCalculator.class).run(args);
+	}
+
+}
